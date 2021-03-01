068d58aaf9e534f79b262950e76a8b2de7e99b8f
LeoGao1 <37646109+LeoGao1@users.noreply.github.com>
2021-03-01 14:06:26 -0800

    Add files via upload

--- run.py
+++ run.py
@@ -5,22 +5,33 @@ import json
 
 sys.path.insert(0, 'src/classifier')
 sys.path.insert(0, 'src/hypothesis_test')
-#sys.path.insert(0, 'src/model')
+sys.path.insert(0, 'src/data_cleaning')
 
 from classifier import build_classifier
 from hypothesis_tests import hypo_test_on_numerical
-
+from clean import data_cleaning
 
 data_path = "cleaned_data/system_sysinfo_unique_normalized.csv000"
+cleaned_data_path = "cleaned_data/"
+all_classifier_list = ['knn','random forest',
+                        'decision tree','neural network',
+                        'svm','sgd','logistic']
+
+result_path = 'result/classifier_performance.csv'
 
 def main(tragets):
+    if 'clean_data' in targets:
+        data_cleaning(data_path,cleaned_data_path)
+
     if 'build-classifier' in targets:
+        data_cleaning(data_path,cleaned_data_path)
         build_classifier(data_path)
 
     if 'hypo-test' in targets:
         hypo_test_on_numerical(data_path,'ram')
 
     if 'test' in targets:
+        data_cleaning(data_path,cleaned_data_path)
         build_classifier(data_path)
 
 if __name__ == '__main__':
