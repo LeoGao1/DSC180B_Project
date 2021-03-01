eb2b6b78985fe277693f1ef4701e12870fb73230
LeoGao1 <weg021@ucsd.edu>
2021-03-01 12:55:07 -0800

    renamed:    system_sysinfo_unique_normalized.csv000 -> cleaned_data/system_sysinfo_unique_normalized.csv000 	modified:   notebooks/Classifiers.ipynb 	new file:   result/classifier_performance.csv 	modified:   run.py 	renamed:    classifier.py -> src/classifier/classifier.py 	renamed:    hypothesis_tests.py -> src/hypothesis_test/hypothesis_tests.py

--- run.py
+++ run.py
@@ -1,16 +1,27 @@
+
+import sys
+import json
+
+
+sys.path.insert(0, 'src/classifier')
+sys.path.insert(0, 'src/hypothesis_test')
+#sys.path.insert(0, 'src/model')
+
 from classifier import build_classifier
 from hypothesis_tests import hypo_test_on_numerical
-import sys
+
+
+data_path = "cleaned_data/system_sysinfo_unique_normalized.csv000"
 
 def main(tragets):
     if 'build-classifier' in targets:
-        build_classifier("system_sysinfo_unique_normalized.csv000")
+        build_classifier(data_path)
 
     if 'hypo-test' in targets:
-        hypo_test_on_numerical("system_sysinfo_unique_normalized.csv000",'ram')
+        hypo_test_on_numerical(data_path,'ram')
 
     if 'test' in targets:
-        build_classifier("system_sysinfo_unique_normalized.csv000")
+        build_classifier(data_path)
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
